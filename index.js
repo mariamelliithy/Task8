298358bd603e73f5b835f1d2df6ff289af4af5b2
mariamelliithy <126201396+mariamelliithy@users.noreply.github.com>
2023-08-07 20:37:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// For Task 1 
+
+// 1 - Inctroduce Your Self (Name , age  , faculty , ) in varriable and write it in HTML Using JS Note using temolate Literal
+
+
+        //Write Code Here
+        // Note : you can write in HTML using  document.write(varribale_Name) ; 
+        const name = "Mariam";
+        const age = 21;
+        const faculty = "Computer Science";
+        const introduction = `My name is ${name}, I am ${age} years old, and I am studying ${faculty}.`;
+        document.write(introduction);
+
+// 2 - You Have Number 
+
+let str = "20000.432423" ; 
+
+console.log("12312312312") //convert String To Number Using to Two different Method
+// Method 1 Using parseFloat()
+console.log(parseFloat(str));
+//method 2 Using Number constructor
+console.log(Number(str));
+
+
+
